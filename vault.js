550d36050283447b0d3d385684a867af19e5225d
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-02 09:24:43 -1000

    First Run

--- vault.js
+++ vault.js
@@ -1,4 +1,17 @@
 'use strict';
 module.exports = function() {
-  
+var secretSecret = {};
+return {
+  setValue : function (key, value){
+    secretSecret[key] = value;
+  },
+
+  getValue : function (key){
+    if (secretSecret.hasOwnProperty(key)){
+      return secretSecret[key];
+    }else{
+      return null;
+    }
+  }
+}
 };
